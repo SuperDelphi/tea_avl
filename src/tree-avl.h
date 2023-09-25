4ebef9b0c08ff9bc6bfd8028cf97d2f885d8bccc
SuperDelphi <superdelphi@protonmail.com>
2023-09-25 16:09:02 +0200

    Ajouté balance + clean code

--- src/tree-avl.h
+++ src/tree-avl.h
@@ -6,62 +6,64 @@
 
 typedef struct _TreeNode *Tree;
 
-struct _TreeNode
-  {
+struct _TreeNode {
     Tree left;
     Tree right;
     char data[1];
-  };
+    size_t balance;
+};
 
-Tree tree_new ();
+Tree tree_new();
 
-void tree_delete (Tree tree, void (*delete) (void *));
+void tree_delete(Tree tree, void (*
 
-Tree tree_create (const void *data, size_t size);
+delete) (void *));
 
-Tree tree_get_left (Tree tree);
+Tree tree_create(const void *data, size_t size);
 
-Tree tree_get_right (Tree tree);
+Tree tree_get_left(Tree tree);
 
-void *tree_get_data (Tree tree);
+Tree tree_get_right(Tree tree);
+
+void *tree_get_data(Tree tree);
 
 bool tree_set_left(Tree tree, Tree left);
 
-bool tree_set_right (Tree tree, Tree right);
+bool tree_set_right(Tree tree, Tree right);
 
 bool tree_set_data(Tree tree, const void *data, size_t
 size);
 
-void tree_pre_order (Tree tree,
-                     void (*func) (void *, void *),
-                     void *extra_data); // VGD
+void tree_pre_order(Tree tree,
+                    void (*func)(void *, void *),
+                    void *extra_data); // VGD
 
-void tree_in_order (Tree tree,
-                    void (*func) (void *, void *),
-                    void *extra_data); // GVD
+void tree_in_order(Tree tree,
+                   void (*func)(void *, void *),
+                   void *extra_data); // GVD
 
-void tree_post_order (Tree tree,
-                      void (*func) (void *, void *),
-                      void *extra_data); // GDV
+void tree_post_order(Tree tree,
+                     void (*func)(void *, void *),
+                     void *extra_data); // GDV
 
-size_t tree_height (Tree tree);
+size_t tree_height(Tree tree);
 
-size_t tree_size (Tree tree);
+size_t tree_size(Tree tree);
 
-bool tree_insert_sorted(Tree * ptree,
+bool tree_insert_sorted(Tree *ptree,
                         const void *data,
                         size_t size,
-                        int (*compare) (const void *, const void *));
+                        int (*compare)(const void *, const void *));
 
-void *tree_search (Tree tree,
-                   const void *data,
-                   int (*compare) (const void *, const
-void *));
+void *tree_search(Tree tree,
+                  const void *data,
+                  int (*compare)(const void *, const
+                  void *));
 
-int tree_sort (void *array,
-               size_t length,
-               size_t size,
-               int (*compare) (const void *, const void
-*));
+int tree_sort(void *array,
+              size_t length,
+              size_t size,
+              int (*compare)(const void *, const void
+              *));
 
 #endif
