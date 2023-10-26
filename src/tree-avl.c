ad1df7ec936e3ae7acd0acaa9717a7f49f0dcc38
Wassimbkl <wbenkhelifa48@gmail.com>
2023-10-26 17:27:34 +0100

    fonction de la suppresion (pas encore testé)

--- src/tree-avl.c
+++ src/tree-avl.c
@@ -24,8 +24,7 @@ Tree tree_create(const void *data, size_t size) {
     if (tree) {
         tree->left = NULL;
         tree->right = NULL;
-        tree->parent = NULL;
-        memcpy(tree->data, data, size);
+     memcpy(tree->data, data, size);
         tree->balance = 0;
     }
 
@@ -207,47 +206,142 @@ int tree_sort(void *array,
 }
 
 // Fonctions ajoutées pour le TEA.
-void rotation_left_delphi(Tree *tree) {
-    if ((*tree)->right != NULL) {
-        (*tree)->left = (*tree);
-        (*tree)->left->parent = (*tree)->right;
-        (*tree)->right->parent = (*tree)->parent;
-//        (*tree)->right->left = (*tree)->left;
-        (*tree) = (*tree)->right;
-    }
+
+void rotation_left(Tree tree) {
+    Tree newParent = tree->left;
+    Tree children = newParent->right;
+
+    newParent->right = tree;
+    tree->left = children;
+
+    tree->balance = tree_height(tree->left) - tree_height(tree->right);
+    newParent->balance = tree_height(newParent->left) - tree_height(newParent->right);
 }
 
-Tree rotate_right_left(Tree node) {
-    if (!node) {
-        return node;
-    }
+void rotation_right(Tree tree) {
+    Tree newParent = tree->right;
+    Tree children = newParent->left;
 
-    // Rotation droite (droite-droite) sur le sous-arbre droit.
-    if (node->right) {
-        node->right = rotate_right(node->right);
-    }
+    newParent->left = tree;
+    tree->right = children;
 
-    // Rotation gauche (gauche-gauche) sur le nœud d'origine.
-    return rotate_left(node);
-}
-Tree rotate_left_right(Tree node) {
-    if (!node) {
-        return node;
-    }
-
-    // Rotation gauche (gauche-gauche) sur le sous-arbre gauche.
-    if (node->left) {
-        node->left = rotate_left(node->left);
-    }
-
-    // Rotation droite (droite-droite) sur le nœud d'origine.
-    return rotate_right(node);
+    tree->balance = tree_height(tree->left) - tree_height(tree->right);
+    newParent->balance = tree_height(newParent->left) - tree_height(newParent->right);
 }
 
-
-void rotation_right_delphi(Tree *tree) {
-//    if (tree->left != NULL) {
-//        tree->right = tree;
-//        tree = tree->left;
+//void rotation_left_delphi(Tree *tree) {
+//    if ((*tree)->right != NULL) {
+//        (*tree)->left = (*tree);
+//        (*tree)->left->parent = (*tree)->right;
+//        (*tree)->right->parent = (*tree)->parent;
+////        (*tree)->right->left = (*tree)->left;
+//        (*tree) = (*tree)->right;
 //    }
+//}
+//
+//Tree rotate_right_left(Tree node) {
+//    if (!node) {
+//        return node;
+//    }
+//
+//    // Rotation droite (droite-droite) sur le sous-arbre droit.
+//    if (node->right) {
+//        node->right = rotate_right(node->right);
+//    }
+//
+//    // Rotation gauche (gauche-gauche) sur le nœud d'origine.
+//    return rotate_left(node);
+//}
+//Tree rotate_left_right(Tree node) {
+//    if (!node) {
+//        return node;
+//    }
+//
+//    // Rotation gauche (gauche-gauche) sur le sous-arbre gauche.
+//    if (node->left) {
+//        node->left = rotate_left(node->left);
+//    }
+//
+//    // Rotation droite (droite-droite) sur le nœud d'origine.
+//    return rotate_right(node);
+//}
+//
+//
+//void rotation_right_delphi(Tree *tree) {
+////    if (tree->left != NULL) {
+////        tree->right = tree;
+////        tree = tree->left;
+////    }
+//}
+static Tree _tree_remove(Tree *ptree, const void *data, size_t size, int (*compare)(const void *, const void *), void (*delete)(void *)) {
+    Tree tree = *ptree;
+
+    if (tree == NULL) {
+        return NULL; // Le nœud à supprimer n'existe pas, retourne NULL.
+    }
+
+    int C = compare(data, tree->data);
+
+    if (C < 0) {
+        tree->left = _tree_remove(&tree->left, data, size, compare, delete);
+    } else if (C > 0) {
+        tree->right = _tree_remove(&tree->right, data, size, compare, delete);
+    } else {
+        // Le nœud à supprimer a été trouvé
+        if (tree->left == NULL) {
+            Tree temp = tree->right;
+            if (delete) {
+                delete(tree->data);
+            }
+            free(tree);
+            return temp;
+        } else if (tree->right == NULL) {
+            Tree temp = tree->left;
+            if (delete) {
+                delete(tree->data);
+            }
+            free(tree);
+            return temp;
+        } else {
+            // Cas où le nœud a deux sous-arbres
+            // Trouver le nœud minimal du sous-arbre droit
+            Tree min_right = tree_search(tree->right, data, compare);
+            // Copier la valeur du nœud minimal dans le nœud à supprimer
+            memcpy(tree->data, min_right->data, size);
+            // Supprimer le nœud minimal du sous-arbre droit
+            tree->right = _tree_remove(&tree->right, min_right->data, size, compare, delete);
+        }
+    }
+
+    if (tree == NULL) {
+        return NULL; // Si l'arbre est devenu vide, retourne NULL
+    }
+
+    // Met à jour le facteur d'équilibre et effectue les rotations si nécessaire
+    tree->balance = tree_height(tree->left) - tree_height(tree->right);
+
+    if (tree->balance > 1) {
+        // Déséquilibre à gauche
+        if (tree_height(tree->left->left) >= tree_height(tree->left->right)) {
+            // Rotation simple à droite
+            rotation_right(tree);
+        } else {
+            // Rotation double à gauche-droite
+            rotation_left(tree->left);
+            rotation_right(tree);
+        }
+    } else if (tree->balance < -1) {
+        // Déséquilibre à droite
+        if (tree_height(tree->right->right) >= tree_height(tree->right->left)) {
+            // Rotation simple à gauche
+            rotation_left(tree);
+        } else {
+            // Rotation double à droite-gauche
+            rotation_right(tree->right);
+            rotation_left(tree);
+        }
+    }
+
+    return tree;
 }
+
